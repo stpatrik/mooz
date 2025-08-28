67ebcb3f1a66252c4da92daad068a72420e15658
stpatrik <37343843+stpatrik@users.noreply.github.com>
2025-08-28 16:46:12 +0200

    merge main

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,8 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-  
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: { open: true },
+})
