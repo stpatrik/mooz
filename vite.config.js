2f7a16d5db99c469ea26b63d0eb508dafa8b80da
stpatrik <37343843+stpatrik@users.noreply.github.com>
2025-08-28 15:35:31 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,4 @@ import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  base: '/mooz/',          // для GitHub Pages: https://stpatrik.github.io/mooz/
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)), // "@/..." -> "src/..."
-    },
-  },
-  // не обязательно, просто удобно
-  server: { open: true },
-})
+  base: '/mooz/',          // для GitHub Pages: https://stpatri
