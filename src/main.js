8124c1ff4d68c243d1da9602dbc3617cf7e65208
stpatrik <37343843+stpatrik@users.noreply.github.com>
2025-08-28 15:27:24 +0200

    fix

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './styles.css'     // <-- добавь эту строку
+import './styles.css'    
 
 createApp(App).mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  base: '/mooz/',   // имя репозитория для GitHub Pages
+  base: '/mooz/',          // для GitHub Pages: https://stpatrik.github.io/mooz/
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)), // ⚡️ алиас
+      '@': fileURLToPath(new URL('./src', import.meta.url)), // "@/..." -> "src/..."
     },
   },
+  // не обязательно, просто удобно
+  server: { open: true },
 })
