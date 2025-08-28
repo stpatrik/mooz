fe1afacd5727ad92916fbf4d481c66fe3d6e73f2
stpatrik <37343843+stpatrik@users.noreply.github.com>
2025-08-28 15:14:46 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
 
 export default defineConfig({
+  base: '/mooz/',   // ⚡ Важно для GitHub Pages
   plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
 })
